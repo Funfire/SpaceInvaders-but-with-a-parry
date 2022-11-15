0196f58efa70d51b42307635a09e31a2713583e6
Funfire <humzanomi@gmail.com>
2022-11-14 20:57:11 -0500

    Made a movement system and shooting

---
+++ Scripts/Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+
+onready var bullet = preload("res://Scenes/Bullet.tscn")
+export (int) var SPEED = 300
+
+func _physics_process(delta):
+	shoot()
+	var velocity = Vector2()
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	velocity = velocity.normalized()
+	move_and_slide(velocity*SPEED)
+
+func shoot():
+	if Input.is_action_just_pressed("shoot"):
+		var b = bullet.instance()
+		get_parent().add_child(b)
+		b.global_position = $Position2D.global_position

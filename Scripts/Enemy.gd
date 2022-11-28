c59a461dc2dd2c4a960b35aaad41a524a744270a
Funfire <humzanomi@gmail.com>
2022-11-28 13:43:59 -0500

    enemies shoot bullet and get hurt by parried

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,6 +1,25 @@
 extends KinematicBody2D
 
+onready var bullet = preload("res://Scenes/EnemyBullet.tscn")
+var canShoot = true
 
+func _physics_process(delta):
+	enemyShoot()
+
+
+func enemyShoot():
+	if canShoot == true:
+		var b = bullet.instance()
+		get_parent().add_child(b)
+		b.global_position = $Position2D.global_position
+		$Timer.start()
+		canShoot = false
+
+
+
+
+func _on_Timer_timeout():
+	canShoot = true
 
 
 
